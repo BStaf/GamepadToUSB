8a93410dd036d741779de66d33e72068aba12698
Bill S <will.staf@gmail.com>
2019-11-23 19:04:21 -0500

    created array of IGameControllers

--- gamePadToUsb.ino
+++ gamePadToUsb.ino
@@ -4,13 +4,19 @@
 #include "Nes.h"
 #include "Genesis.h"
 
-Nes *nes = new Nes(10,16,14);
-Genesis *genesis = new Genesis(3, 4, 5, 6, 7, 8, 9);
-Gamepad *gamepad = new Gamepad(nes);
+IGameController *gameController[2];
+//*gameController = new Genesis(3, 4, 5, 6, 7, 8, 9);
+//Nes *nes = new Nes(10,16,14);
+//Genesis *genesis;// = new Genesis(3, 4, 5, 6, 7, 8, 9);
+//genesis = new Genesis(3, 4, 5, 6, 7, 8, 9);
+Gamepad *gamepad;// = new Gamepad(nes);
  
 //Gamepad gamepad;
 
 void setup() {
+  gameController[0] = new Nes(10,16,14);
+  gameController[1] = new Genesis(3, 4, 5, 6, 7, 8, 9);
+  gamepad = new Gamepad(gameController[0]);
   //Serial.begin(9600); 
   //Nes *nes = new Nes(10,16,14);
   //Genesis *genesis = new Genesis(3, 4, 5, 6, 7, 8, 9);
